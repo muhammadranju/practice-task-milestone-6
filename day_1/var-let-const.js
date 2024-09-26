ad02efe80d2a4c6598f478f75fdfb2cc8d58aa39
Muhammad Ranju <mdranju23@gmail.com>
2024-09-26 07:50:39 +0600

    feat: arrow function working

--- day_1/var-let-const.js
+++ day_1/var-let-const.js
@@ -1,4 +1,8 @@
-const money = 25;
+// const money = 25;
 
-const rich = money * 2;
-console.log(rich);
+// const rich = money * 2;
+// console.log(rich);
+
+// const arrow = () => "it's working 🔥😲";
+
+// console.log(arrow());
