592174f68166c427d32f21ad4508f8f34f5b30df
Muhammad Ranju <mdranju23@gmail.com>
2024-09-26 08:29:37 +0600

    Refactor: Update code to demonstrate array spread

--- day_1/var-let-const.js
+++ day_1/var-let-const.js
@@ -3,6 +3,16 @@
 // const rich = money * 2;
 // console.log(rich);
 
-const arrow = () => "it's working 🔥😲";
+// const arrow = () => "it's working 🔥😲";
 
-console.log(arrow());
+// console.log(arrow());
+// return;
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const num2 = [...num];
+num2.push(11);
+// console.log(num2);
+// console.log(num);
+
+console.table({ num, num2 });
