a5db6ca06fdcf8da86b62824d0d2c60bbd654689
Muhammad Ranju <mdranju23@gmail.com>
2024-09-26 09:01:04 +0600

    Refactor: Restructure and move code in day_1
    - Move code from var-let-const.js to index.js
    - Delete var-let-const.js
    - Update package-lock.json
    - Remove commented out code
    - Improve code organization

---
+++ day_1/index.js
@@ -0,0 +1,44 @@
+// const money = 25;
+
+// const rich = money * 2;
+// console.log(rich);
+
+// const arrow = () => "it's working 🔥😲";
+
+// console.log(arrow());
+// return;
+
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const num2 = [...num];
+// num2.push(11);
+// // console.log(num2);
+// // console.log(num);
+
+// console.table({ num, num2 });
+
+// const glass = {
+//   name: "coffee",
+//   color: "red",
+//   price: 10,
+//   quantity: 10,
+// };
+
+// console.log(glass);
+
+// // give me the keys
+// const keys = Object.keys(glass);
+// console.log(keys);
+
+// // give me the values
+// const values = Object.values(glass);
+// console.log(values);
+
+// // give me the entries
+// const entries = Object.entries(glass);
+// console.log(entries);
+
+// const freeze = Object.freeze(glass);
+// console.log(freeze);
+// glass.name = "tea";
+// console.log(glass);
