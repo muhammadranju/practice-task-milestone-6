c5fdef36c39c2fce945f63c357e0046036658b5c
Muhammad Ranju <mdranju23@gmail.com>
2024-09-29 10:51:59 +0600

    Refactor: Remove previous code and add new code for testing

--- day_3/index.js
+++ day_3/index.js
@@ -16,4 +16,39 @@ function sayHello(fn, name) {
   fn(name);
 }
 
-sayHello(myName, "Ranju");
+// sayHello(myName, "Ranju");
+
+function a(a, b, c) {
+  const args = [...arguments];
+  console.log(args);
+}
+
+// a(1, 2, 3, 4, 5);
+let p = "Javascript";
+let q = p;
+p = "React";
+// console.log(q);
+
+const isTrue = true;
+// console.log(!isTrue ? "hello" : "world");
+
+const sum = (p, q) => {
+  p + q;
+};
+const result = sum(2, 3);
+// console.log(result);
+
+if ("2" === 2) {
+  console.log("Inside if");
+} else {
+  //   console.log("Inside else");
+}
+
+function work(x, y = 4) {
+  names = "Ranju";
+  return x + y;
+}
+// console.log(work(32));
+work();
+
+console.log(names);
